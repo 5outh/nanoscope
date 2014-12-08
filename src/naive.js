289d5666cc88a2728f6d073814d0d479d24e2e31
Benjamin Kovach <Ben@Benjamins-MacBook-Pro.local>
2014-12-07 20:51:34 -0500

    Slightly less naive

--- src/naive.js
+++ src/naive.js
@@ -1,19 +1,32 @@
 
 var _ = require('lodash'),
+    constant,
     NaiveLens;
 
-NaiveLens = function (obj, get, set) {
+/**
+ * Discards the second argument and returns the first.
+ *
+ * @param a
+ * @returns {Function}
+ */
+constant = _.curry(function(a, b) { return a; });
+
+NaiveLens = function (obj, get, over) {
     this._object = obj;
     this._get = get;
-    this._set = set;
+    this._over = over;
 };
 
 NaiveLens.prototype.get = function () {
     return this._get(this._object);
 };
 
+NaiveLens.prototype.over = function (func) {
+    return this._over(this._object, func);
+};
+
 NaiveLens.prototype.set = function (val) {
-    return this._set(this._object, val);
+    return this._over(this._object, constant(val));
 };
 
 var testJS = {
@@ -27,9 +40,9 @@ var testLens = new NaiveLens(
     function (obj) {
         return obj.a.b;
     },
-    function (obj, val) {
+    function (obj, func) {
         var newObj = _.cloneDeep(obj);
-        newObj.a.b = val;
+        newObj.a.b = func(newObj.a.b);
         return newObj;
     });
 
