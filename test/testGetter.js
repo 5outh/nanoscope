4fc12335170310c6950fdaafd0b2b82914e3155b
Benjamin Kovach <bkovach5@uga.edu>
2014-12-10 16:39:56 -0500

    Add Get/Set Combo tests

--- test/testGetter.js
+++ test/testGetter.js
@@ -2,7 +2,9 @@
 
 var _ = require('lodash'),
     IndexedLens = require('../src/array/IndexedLens'),
-    Getter = require('../src/Getter');
+    Getter = require('../src/Getter'),
+    Setter = require('../src/Setter'),
+    IdLens = require('../src/IdLens');
 
 describe('Getter', function () {
     var testArr, testLens;
@@ -37,6 +39,26 @@ describe('Getter', function () {
         it('should throw an error', function () {
             try {
                 getter.over(testArr, function () {});
+            } catch (ex) {
+                ex.message.should.equal('over not permitted in a Getter');
+            }
+        });
+    });
+
+    describe('Get/Set Combo', function () {
+        var getSetLens = Getter.fromLens(Setter.fromLens(new IdLens()));
+
+        it('should fail to get', function () {
+            try {
+                getSetLens.get(100);
+            } catch (ex) {
+                ex.message.should.equal('get not permitted in a Setter');
+            }
+        });
+
+        it('should fail to set', function () {
+            try {
+                getSetLens.set(100, 200);
             } catch (ex) {
                 ex.message.should.equal('over not permitted in a Getter');
             }
